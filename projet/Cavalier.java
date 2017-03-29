75d55a59d867e71f1f30c336e844bbd6dfb91c55
GISSELAIRE LUCAS p1410766 <p1410766@b710l0410.univ-lyon1.fr>
2017-03-29 14:23:22 +0200

    Ajout des pièces

---
+++ projet/Cavalier.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package projet;
+
+/**
+ *
+ * @author p1410766
+ */
+public class Cavalier extends Piece {
+    
+    public Cavalier(couleur c) {
+        super(c);
+    }
+    
+}
